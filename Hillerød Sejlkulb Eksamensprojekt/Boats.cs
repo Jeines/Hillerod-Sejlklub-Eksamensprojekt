a335dc26dcd455ae21362644b2c59dd93368611d
Mikkel Kornerup Andersen <Mkkmkm75@gmail.com>
2024-12-03 10:31:28 +0100

    Class Boats

--- Hillerød Sejlkulb Eksamensprojekt/Boats.cs
+++ Hillerød Sejlkulb Eksamensprojekt/Boats.cs
@@ -8,5 +8,26 @@ namespace Hillerød_Sejlkulb_Eksamensprojekt
 {
     public class Boats
     {
+        public string Name {  get; set; }
+        public int BoatId {  get; set; }
+        public string Description { get; set; }
+        public double Lenght { get; set; }
+        public double Width { get; set; }
+        public double Mast { get; set; }
+        public int Weight { get; set; }
+
+        public Boats(int boatId, string name, double lenght, double width, double mast, int weight, string description)
+        {
+            Name = name;
+            BoatId = boatId;
+            Lenght = lenght;   
+            Width = width;
+            Mast = mast;
+            Weight = weight;
+            Description = description;
+        }
+
+
+
     }
 }
