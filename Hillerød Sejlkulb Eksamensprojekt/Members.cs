e3845767c85465ed28a1bd69b8f5884f8483067b
Rasmus Emil Olsen <rao003@edu.zealand.dk>
2024-12-03 11:03:16 +0100

    Members

--- Hillerød Sejlkulb Eksamensprojekt/Members.cs
+++ Hillerød Sejlkulb Eksamensprojekt/Members.cs
@@ -8,5 +8,30 @@ namespace Hillerød_Sejlkulb_Eksamensprojekt
 {
     public class Members
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+
+
+        public Members(int id, string name, string lastname, int phonenumber, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastname;
+            PhoneNumber = phonenumber;
+            Email = email;
+            Address = address;
+
+        }
+
+        public override string ToString()
+        {
+            return "Memberid " + Id + "Name " + Name + " " + LastName + " " + PhoneNumber + " " + Email + " " + Address;
+        }
     }
+    
 }
