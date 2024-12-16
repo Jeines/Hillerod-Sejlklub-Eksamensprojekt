627e9928ebb070e56e377daa91ff161abf58f55b
Mikkel Kornerup Andersen <Mkkmkm75@gmail.com>
2024-12-16 12:27:14 +0100

    start på RazorPage

---
+++ HillerødRazorPage/Pages/Boat/GetAllBoats.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødRazorPage.Pages.Boat
+{
+    public class GetAllBoatsModel : PageModel
+    {
+
+//        public List<Models.Boat> boatList = new List<Models.Boat>()
+//        {
+//            new Models.Boat(1, "Optimist ", 2.31, 1.13, 2.25, 35, "Crew 1 person.", "The Optimist is one of the most popular singel-handed sailing dinghies in the world, designed specifically for young sailors up to the age of 15. The Optimist is made out of fiberglass. ", "none")
+//            new Models.Boat(2, "Tera  ", 2.87, 1.23, 2.37, 39, " Crew 1 person. ", "The Tera is a modern dinghy desingned for young sailors, typically from the age 8 and up. The Tera is made out of polyethylene. ", "none ")
+//            new Models.Boat(3, "Feva ", 3.64, 1.42, 2.41, 63, "Crew 1-2 people. ", " The Feva is a very popular dinghy that can be sailed by one or two people. The Feva is made out of polyethylene. ", "none")
+//            new Models.Boat(4, "Laser ", 4.23, 1.42, 3.38, 59, "Crew 1 person. ", "The Laser is one of the most popular single-handed dinghies in the world, used for both relaxed sailing and racing. The laser is made out of fiberglass-reinforced plastic ", "none")
+//            new Models.Boat(5, "Europe ", 3.35, 1.38, 4.8, 60, "Crew 1 person. ", "The Europe dinghy is a popular single-handed dinghy ideal for sailors who have outgrown the Optimist. The Europe is made out of fiberglass", "none")
+//            new Models.Boat(6, "Snipe ", 4.72, 1.52, 4.9, 172, "Crew 2 people.", "The Snipe dinghy is a classic two-person dinghy known for its tacticla sailing and durability. The snipe is made out of fiberglass. ", "none")
+//            new Models.Boat(7, "Wayfarer ", 4.82, 1.85, 5.2, 167, "Crew 2-4 people.", "The Wayfarer dinghy is a versatile and robust sailing dinghy, popular for both racing, cruising, and as training boat. The Wayfarer is made out of fiberglass ", "none")
+//            new Models.Boat(8, "Lynæs ", 4, 1.55, 4.2, 150, "Crew 1-2 people.", " The Lynæs dinghy is a traditinal Danish boat known for its seaworthiness and robust construction. The Lynæs is traditinally made out of oak but the modern version is made out of fiberglass. ", "none")
+
+
+//        };
+
+
+
+
+
+
+
+
+//        //public List<Models.Boat> boatList = new List<Models.Boat>();
+
+
+
+
+            
+        
+   }
+}
---
+++ HillerødRazorPage/Pages/FooterInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødRazorPage.Pages
+{
+    public class FooterInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
